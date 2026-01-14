7c11b67e99a689458d184949b0bb4a1092a2f32a
uchedamian <uche_damian@yahoo.com>
2026-01-14 05:12:47 -0800

    Create authRoutes.js

---
+++ config/models/controllers/controllers/routes/authRoutes.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const c = require("../controllers/authController");
+
+router.post("/register", c.register);
+router.post("/login", c.login);
+router.post("/guest", c.guest);
+
+module.exports = router;
